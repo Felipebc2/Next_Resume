f3cf444f159efe36a5eedc37f152a775240bab08
felipebc2 <felipeb.castro02@gmail.com>
2025-08-11 19:01:26 -0300

    add nav bar -- 0.2

--- src/app/components/HeroFelipe.tsx
+++ src/app/components/HeroFelipe.tsx
@@ -4,7 +4,16 @@ import Shapes from "./Shapes";
 export default function HeroFelipe() {
   return (
     <section className={heroStyles.page}>
-      <Shapes />
+      <header className={heroStyles.header}>
+          <nav className={heroStyles.nav}>
+              <a href="#" className={heroStyles.active}> Home</a>
+              <a href="#" >Services</a>
+              <a href="#" >Skills</a>
+              <a href="#" >Education</a>
+              <a href="#" >Experience</a>
+              <a href="#" >Contact</a>
+          </nav>
+      </header>
       <main className={heroStyles.main}>
         <ol>
           <h1>Portfolio Website 0.1</h1>
