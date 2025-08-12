f8922383ab57e9619bbeeba9040ee7df150b3679
felipebc2 <felipeb.castro02@gmail.com>
2025-08-12 17:10:46 -0300

    Hover Effect -- 0.2

--- src/app/components/Shapes.tsx
+++ src/app/components/Shapes.tsx
@@ -3,7 +3,8 @@ import styles from "../styles/Shapes.module.css";
 export default function Shapes() {
   return (
     <>
-      <div className={styles.shapes}></div>
+      <div className={styles.shapesPink}></div>
+      <div className={styles.shapesBlue}></div>
       <div className={styles.particules}>
         {Array.from({ length: 15 }, (_, i) => (
           <div key={i} className={styles.particle} />
