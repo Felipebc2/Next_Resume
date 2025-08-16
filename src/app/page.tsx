1f8cfe7ee54548b0ad78ce55468495477c7f282d
fbc2 <felipeb.castro02@gmail.com>
2025-08-16 17:49:38 -0300

    Adding Bg-Gradient Parallax -- v1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import styles from "./page.module.css";
 import Hero from "./components/hero";
 import Projects from "./components/projects";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Hero />
+        <Projects />
+        <Projects />
+        <Projects />
         <Projects />
       </main>
     </div>
