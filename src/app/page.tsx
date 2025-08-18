5032c0f2cc5f38da14e7da097f0c638ba3d1ca8b
felipebc2 <felipeb.castro02@gmail.com>
2025-08-18 10:59:59 -0300

    projects-div: MagicBento.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero";
 import styles from "./page.module.css";
+import MagicBento from "@/blocks/Components/MagicBento/MagicBento";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Hero />
+        <MagicBento />
       </main>
     </div>
   );
