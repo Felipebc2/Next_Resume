59a595bd497f1c43a8d751cb6c9361e017d2669d
felipebc2 <felipeb.castro02@gmail.com>
2025-09-01 16:35:06 -0300

    Adding correct icons

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -6,12 +6,24 @@ import { useState } from "react";
 
 import {
     Globe,
-    Database,
-    Smartphone,
     Code,
     Users,
     Eye,
-    EyeOff
+    EyeOff,
+    Handshake,
+    Flag,
+    NotebookPen,
+    Bug,
+    LampDesk,
+    Speech,
+    Brain,
+    Shield,
+    DatabaseZap,
+    Microscope,
+    FolderLock,
+    Bot,
+    Plus,
+    Braces
 } from "lucide-react";
 
 const mainSkills = [
@@ -31,7 +43,7 @@ const mainSkills = [
 const hardSkills = [
     {
         category: "Linguagens de Programação",
-        icon: <Globe className="w-6 h-6" />,
+        icon: <Code className="w-6 h-6" />,
         technologies: ["C", "C++", "C#", "Python", "Java", "JavaScript", "TypeScript", "Lua"]
     },
     {
@@ -41,32 +53,32 @@ const hardSkills = [
     },
     {
         category: "Backend",
-        icon: <Database className="w-6 h-6" />,
+        icon: <Braces className="w-6 h-6" />,
         technologies: ["Nest.js", "Flask", "Node.js"]
     },
     {
-        category: "Data",
-        icon: <Smartphone className="w-6 h-6" />,
+        category: "Database",
+        icon: <DatabaseZap className="w-6 h-6" />,
         technologies: ["MongoDB", "MySql", "Redis", "PostgreSQL", "Cassandra", "Supabase"]
     },
     {
-        category: "Data",
-        icon: <Code className="w-6 h-6" />,
+        category: "Data Science",
+        icon: <Microscope className="w-6 h-6" />,
         technologies: ["Kafka", "SkLearn", "Machine Learning", "Estatística", "LLMs"]
     },
     {
         category: "DevOps, Segurança & Infra",
-        icon: <Code className="w-6 h-6" />,
+        icon: <FolderLock className="w-6 h-6" />,
         technologies: ["Docker", "Microsoft Azure", "Vercel", "Linux", "Firebase", "Wireshark", "LGPD"]
     },
     {
         category: "Automação",
-        icon: <Code className="w-6 h-6" />,
+        icon: <Bot className="w-6 h-6" />,
         technologies: ["Power Automate", "N8N"]
     },
     {
         category: "Extras",
-        icon: <Code className="w-6 h-6" />,
+        icon: <Plus className="w-6 h-6" />,
         technologies: ["Git/GitHub", "Excel", "Unity/Unreal", "Maya3D/Blender", "Arduino/Rapberry", "Canva/Figma"]
     }
 ];
@@ -75,42 +87,42 @@ const hardSkills = [
 const softSkills = [
     {
         category: "Comunicação",
-        icon: <Users className="w-6 h-6" />,
+        icon: <Handshake className="w-6 h-6" />,
         technologies: ["Proatividade", "Trabalho em Equipe", "Networking", "Abertura a Feedbacks"]
     },
     {
         category: "Liderança",
-        icon: <Users className="w-6 h-6" />,
+        icon: <Flag className="w-6 h-6" />,
         technologies: ["Tomada de Decisão", "Colaboração em Times Multidisciplinares", "Gerenciamento de Conflitos", "Gestão de Equipes"]
     },
     {
         category: "Organização",
-        icon: <Users className="w-6 h-6" />,
+        icon: <NotebookPen className="w-6 h-6" />,
         technologies: ["Gerenciamento de Prioridades", "Entregas Rápidas", "Pensamento Estratégico e Analítico", "Gestão de Tempo"]
     },
     {
         category: "Resolução de Problemas",
-        icon: <Users className="w-6 h-6" />,
+        icon: <Bug className="w-6 h-6" />,
         technologies: ["Base Lógica Sólida", "Atenção a Detalhes", "Flexibilidade"]
     },
     {
         category: "Criatividade",
-        icon: <Users className="w-6 h-6" />,
+        icon: <LampDesk className="w-6 h-6" />,
         technologies: ["Desenvolvimento de Ideias", "Criatividade em Soluções Técnicas", "Inovação em Hackathons"]
     },
     {
         category: "Oratória",
-        icon: <Users className="w-6 h-6" />,
+        icon: <Speech className="w-6 h-6" />,
         technologies: ["Pitchs bem desenvolvidos", "Apresentação Técnica", "Clareza na Comunicação", "Paciente"]
     },
     {
         category: "Aprendizado",
-        icon: <Users className="w-6 h-6" />,
+        icon: <Brain className="w-6 h-6" />,
         technologies: ["Mentalidade de Crescimento", "Busca Ativa", "Aprendizado Rápido"]
     },
     {
         category: "Resiliência",
-        icon: <Users className="w-6 h-6" />,
+        icon: <Shield className="w-6 h-6" />,
         technologies: ["Adaptável", "Dedicado em Desafios", "Bom com Mudanças"]
     }
 
